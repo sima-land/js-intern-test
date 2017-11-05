e76045ce373ae56765bdd2aee9a3ac32c578de39
enflaer <enflaer@gmail.com>
2017-11-06 00:32:16 +0500

    #2

--- js/drum-sampler/drum-machine.js
+++ js/drum-sampler/drum-machine.js
@@ -1,47 +1,22 @@
-var key_a = 65;
-var key_s = 83;
-var key_d = 68;
-var key_f = 70;
-var key_g = 71;
-var key_h = 72;
-var key_j = 74;
-var key_k = 75;
-var key_l = 76;
+var keys = [];
+var audioKeys = [];
+var audioSrc = [];
 
-function playSound(url) {
-    var s = new Audio();
-    s.src = url;
-    s.play();
-}
+$(key).each(function() {
+    keys.push($(this).data('key'));
+});
+$(audioKey).each(function() {
+    audioKeys.push($(this).data('key'));
+});
+$(audioKey).each(function() {
+    audioSrc.push($(this).data('src'));
+});
 
 document.onkeydown = function(e) {
-    switch (e.keyCode) {
-        case key_a:
-            playSound("sounds/clap.wav")
+    for (var i = 0; i < keys.length; i++) {
+        if (e.keyCode == keys[i] && audioKeys[i]) {
+            new Audio(audioSrc[i]).play();
             break;
-        case key_s:
-            playSound("sounds/hihat.wav")
-            break;
-        case key_d:
-            playSound("sounds/kick.wav")
-            break;
-        case key_f:
-            playSound("sounds/openhat.wav")
-            break;
-        case key_g:
-            playSound("sounds/boom.wav")
-            break;
-        case key_h:
-            playSound("sounds/ride.wav")
-            break;
-        case key_j:
-            playSound("sounds/snare.wav")
-            break;
-        case key_k:
-            playSound("sounds/tom.wav")
-            break;
-        case key_l:
-            playSound("sounds/tink.wav")
-            break;
+        }
     }
 }

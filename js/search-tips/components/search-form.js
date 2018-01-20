3c8f50eb1caad3f7b7735b0b4259d63a4f624710
dimaname <dimaname@gmail.com>
2018-01-21 00:05:44 +0500

    mvp

--- js/search-tips/components/search-form.js
+++ js/search-tips/components/search-form.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import SearchTips from '../components/search-tips';
 
-const SearchForm =
-  () =>
-    (
-      <section>
-        <h1>Поисковые подсказки</h1>
-        <input className="search-input" type="search"/>
-        <SearchTips />
-      </section>
-    );
+export class SearchForm extends React.Component{
+  constructor(props) {
+		super(props);
+	    this.state = {
+	      searchString: '',
+	    };
 
-export default SearchForm;
+        this.handleChange = this.handleChange.bind(this);      
+        this.handleSelect = this.handleSelect.bind(this);      
+    }
+    
+    render() {
+    	return (  
+	    	<section>
+	        <h1>Поисковые подсказки</h1>
+	        <input 
+	        	className="search-input" 
+	        	type="search"  
+	        	ref={(input) => { this.searchInput = input; }} 
+	        	value={this.state.searchString}			     
+				onChange={this.handleChange} />
+	        <SearchTips words={this.props.searchTipsWords} onSelect={this.handleSelect}/>
+	      </section>)
+    }
+
+    handleChange(event){
+    	const target = event.target;   
+	  	
+	    this.setState({
+	      'searchString': target.value
+	    });
+	    this.props.updateSearchTips(target.value);
+    }  
+    handleSelect(newSearchString){    	
+	    this.setState({
+	      'searchString': newSearchString
+	    });
+	    this.searchInput.focus();
+	    this.props.updateSearchTips();
+    }
+    
+}
+
--- js/search-tips/components/search-tips.js
+++ js/search-tips/components/search-tips.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-const SearchTips = (props) => (
-  <div className="search-tips"></div>
-);
 
-export default SearchTips;
+export default class SearchTips extends React.Component{
+    constructor(props) {
+		super(props); 
+            
+    }
+    
+    render() {
+    	const words = this.props.words;
+    	return (  
+	    	<div className="search-tips">
+			{words.map((word, index) => {
+			    return (<div onClick={this.handleClick.bind(this, word)}  key={index}>{word}</div>);
+			})}
+	    	</div>
+	    )
+    } 
+
+    handleClick(word){
+    	this.props.onSelect(word);
+    }
+    
+}
+

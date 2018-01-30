c9fffa3b23baf320a12555fcc3b1f3d176583a61
Aletagro <Aletagro@yandex.ru>
2018-01-30 21:49:50 +0500

    search-tips

--- js/search-tips/reducers/search-reducer.js
+++ js/search-tips/reducers/search-reducer.js
@@ -1,7 +1,18 @@
 const search = (state = {}, action) => {
   switch (action.type) {
     case 'search':
-      return {};
+      let words = state.words || [];
+      let tips = [];
+      let query = action.payload.trim();
+      if (query.length > 0) {
+        for (let i = 0; i < words.length && tips.length < 5; i++) {
+          if (words[i]
+          && words[i].toLowerCase().indexOf(query.toLowerCase()) === 0) {
+            tips.push(words[i]);
+          }
+        }
+      }
+      return Object.assign({}, state, { tips });
     default:
       return state;
   }

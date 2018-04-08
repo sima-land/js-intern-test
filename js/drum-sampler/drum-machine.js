a3eeae9fb239d09d90f6d0400798360ee713c1b9
EkaterinaZyuzeva <36573403+EkaterinaZyuzeva@users.noreply.github.com>
2018-04-08 14:47:00 +0500

    Add files via upload
    Поправки кода

--- js/drum-sampler/drum-machine.js
+++ js/drum-sampler/drum-machine.js
@@ -1,29 +1,37 @@
 window.onload = function () {
     var allAudio = document.querySelectorAll('audio');
     var allKeys = document.querySelectorAll('.key');
-    
-    window.addEventListener('keydown', makeSound);
-    
-    allKeys.forEach(function (key) {
-        key.addEventListener('click', makeSound);
-    })
-    allKeys.forEach(function (key) {
-        key.addEventListener('transitionend', function (e) {
-            if (e.propertyName !== 'transform') return;
-            this.classList.remove('playing');
-        });
-    });
 
     function makeSound(e) {
         for (var i = 0; i < allAudio.length; i++) {
             allAudio[i].pause();
         }
-        var code = e.keyCode ? e.keyCode : this.getAttribute("data-key");
+        var code;
+        if (e.keyCode) {
+            code = e.keyCode;
+        }
+        else {
+            code = this.getAttribute("data-key");
+        }
         var activeKey = document.querySelector('.key[data-key="' + code + '"]');
         var activeAudio = document.querySelector('audio[data-key="' + code + '"]');
-        if (!activeAudio) return;
+        if (!activeAudio) {
+            return;
+        }
         activeAudio.currentTime = 0;
         activeAudio.play();
         activeKey.classList.add('playing');
     }
-}
+    window.addEventListener('keydown', makeSound);
+    allKeys.forEach(function (key) {
+        key.addEventListener('click', makeSound);
+    });
+    allKeys.forEach(function (key) {
+        key.addEventListener('transitionend', function (e) {
+            if (e.propertyName !== 'transform') {
+                return;
+            }
+            this.classList.remove('playing');
+        });
+    });
+};

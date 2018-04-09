69dace683819f364b335f94fb4ff4cfe156d7bc9
EkaterinaZyuzeva <36573403+EkaterinaZyuzeva@users.noreply.github.com>
2018-04-09 16:47:53 +0500

    Add files via upload
    Разбиение функции makeSound()

--- js/drum-sampler/drum-machine.js
+++ js/drum-sampler/drum-machine.js
@@ -1,15 +1,14 @@
 window.onload = function () {
     var allAudio = document.querySelectorAll('audio');
     var allKeys = document.querySelectorAll('.key');
-    
     var all = [];
-    for(var i = 0; i<allAudio.length; i++){
+    for (var i = 0; i < allAudio.length; i++) {
         var audio = {
-            id: allAudio[i].dataset.key,
-            audio: allAudio[i]
+            id: allAudio[i].dataset.key
+            , audio: allAudio[i]
         };
-        for(var r=0; r<allKeys.length; r++){
-            if(allKeys[r].dataset.key === audio.id){
+        for (var r = 0; r < allKeys.length; r++) {
+            if (allKeys[r].dataset.key === audio.id) {
                 audio.button = allKeys[r];
             }
         }
@@ -17,43 +16,41 @@ window.onload = function () {
     }
     var active = {};
 
-    function setNewAudio(code){
+    function setNewAudio(code) {
         var newAudio = {};
-        all.forEach(function(element){
-            if(element.id === code){
+        all.forEach(function (element) {
+            if (element.id === code) {
                 newAudio.audio = element.audio;
                 newAudio.button = element.button;
             }
         });
-
         return newAudio;
     }
-    
-    function makeSound(e) {
-        var code = e.keyCode ? e.keyCode : e.dataset.key;
-        var newAudio = setNewAudio(code.toString());
-        
-        if (!newAudio.audio || !newAudio.button) {
-            return;
-        }
-        
-        if(active.audio){
+
+    function makeSound(newAudio) {
+        if (active.audio) {
             active.audio.pause();
         }
-        
         active = newAudio;
         active.audio.currentTime = 0;
         active.audio.play();
         active.button.classList.add('playing');
     }
-    window.addEventListener('keydown', makeSound);
-    
-    window.onclick = function(e){
+
+    function setAudio(e) {
+        var code = e.keyCode ? e.keyCode : e.dataset.key;
+        var newAudio = setNewAudio(code.toString());
+        if (!newAudio.audio || !newAudio.button) {
+            return;
+        }
+        makeSound(newAudio);
+    }
+    window.addEventListener('keydown', setAudio);
+    window.onclick = function (e) {
         var target = e.target;
         var button = target.closest('.key');
-        makeSound(button);
-    }
-    
+        setAudio(button);
+    };
     allKeys.forEach(function (key) {
         key.addEventListener('transitionend', function (e) {
             if (e.propertyName !== 'transform') {

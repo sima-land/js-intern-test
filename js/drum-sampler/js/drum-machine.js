bbbe5fcdfb160cc8a04c8ac3087e45f2eda99870
elijah0107 <38666717+elijah0107@users.noreply.github.com>
2018-04-29 14:07:24 +0500

    Update drum-machine.js
    Не заметил стиль .playing. Так проще, можно убрать библиотеку

--- js/drum-sampler/js/drum-machine.js
+++ js/drum-sampler/js/drum-machine.js
@@ -1,66 +1,43 @@
-        var currentAudio = new Audio;
-
-        function sound(link){
-            currentAudio.pause();
-            var audio = new Audio();
-            audio.src = 'sounds/'+ link +'.wav';
-            audio.autoplay = true;
-            currentAudio = audio;
-            return currentAudio;
-        }
-        
-        function playSound(e){
-     
-            var root = document.getElementById("root");
-            
-            function animation(numb){
-                var big = anime({
-                    targets: '.key:nth-child('+ numb +')',
-                    scale: 1.2,
-                    duration: 50,
-                    direction: 'alternate'
-                });
+        var sounds = document.querySelectorAll('audio');
+        var keys = document.querySelectorAll('.key');
+        var all = {};
+        for (var i = 0; i < sounds.length; i++) {
+            var id = sounds[i].dataset.key;
+            var value = {
+                audio: sounds[i]
+            };
+            for (var j = 0; j < keys.length; j++) {
+                if (keys[j].dataset.key === id) {
+                    value.button = keys[j];
+                }
             }
-            
-            switch(e.keyCode){
-
-                case 65:// если нажата клавиша a
-                        sound('clap');
-                        animation(1);
-                    break;
-                case 83:  // если нажата клавиша s
-                        sound('hihat');
-                        animation(2);
-                    break;
-                case 68:
-                        sound('kick');
-                        animation(3);// если нажата клавиша d
-                    break;
-                case 70:   // если нажата клавиша f
-                        sound('openhat');
-                        animation(4);
-                    break;
-                case 71:   // если нажата клавиша g
-                        sound('boom');
-                        animation(5);
-                    break;
-                case 72:   // если нажата клавиша h
-                        sound('ride');
-                        animation(6);
-                    break;
-                case 74:   // если нажата клавиша j
-                        sound('snare');
-                        animation(7);
-                    break;
-                case 75:   // если нажата клавиша k
-                        sound('tom');
-                        animation(8);
-                    break;
-                case 76:   // если нажата клавиша l
-                        sound('tink');
-                        animation(9);
-                    break;
-            }
+            all[id] = value;
         }
- 
-addEventListener("keydown", playSound);
+        var activeButton = {};
+        function increaseBlock() {
+            activeButton.button.classList.add('playing');
+            activeButton.button.addEventListener('transitionend', function (e) {
+            if (e.propertyName !== 'transform') {
+                return;
+            }
+            this.classList.remove('playing');
+        });
+        }
+        function playTracks(createAudio) {
+            if (activeButton.audio) {
+                activeButton.audio.pause();
+            }
+            activeButton = createAudio;
+            activeButton.audio.currentTime = 0;
+            activeButton.audio.play();
+        }
+        function playAudio(e) {
+            var code = e.keyCode ? e.keyCode : e.dataset.key;
+            var createAudio = all[code];
+            if (!createAudio) {
+                return;
+            }
+            playTracks(createAudio);
+            increaseBlock();
+        }
+        addEventListener("keydown", playAudio);

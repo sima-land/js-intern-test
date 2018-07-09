a57450ec9d9f337133e9cab379b60545f34c964b
komelskikh95 <curren.8139@yandex.ru>
2018-07-09 16:43:41 +0500

    chenges

--- js/drum-sampler/drum-machine.js
+++ js/drum-sampler/drum-machine.js
@@ -1,12 +1,11 @@
 $(document).keydown(function(e){
 	        k = e.keyCode;
-   			a = $('.key').attr('data-key');
-	        $('audio').each(function(){
-				    this.pause();
-				    this.currentTime = 0;
-			});
-	        		$('audio[data-key=' + k + ']').get(0).play();
-	        		$('.key[data-key=' + k + ']').addClass('playing');
+	        $('audio:not([data-key=' + k + '])').each(function(){
+				  this.pause();
+				  this.currentTime = 0;
+				});
+	        $('audio[data-key=' + k + ']').get(0).play();
+	        $('.key[data-key=' + k + ']').addClass('playing');
 	    });
 $(document).keyup(function(){
 	   		$('.key').removeClass('playing');

caa3118787f9b1860e74779997a9a6761b921643
Justiffy <Shnap2010@yandex.ru>
2018-09-06 19:51:41 +0500

    testovoe

---
+++ js/drum-sampler/drum-machine.js
@@ -0,0 +1,21 @@
+let mySound = new Audio();
+
+const keyDown = event => {
+  const keyCode = event.keyCode;
+
+  mySound.pause();
+  addClass(keyCode);
+  mySound = document.querySelector(`audio[data-key='${keyCode}']`);
+  mySound.play();
+};
+
+const addClass = keyCode => {
+  const elem = document.querySelector(`div[data-key='${keyCode}']`);
+
+  elem.classList.add('playing');
+  setTimeout(() => {
+    elem.classList.remove('playing');
+  }, 100);
+};
+
+document.addEventListener('keydown', keyDown);

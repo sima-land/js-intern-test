3646b23a4caf8057633746c4660df7fe0df97b2d
Justiffy <shnap2010@yandex.ru>
2018-09-08 11:39:54 +0500

    task/Popkov_v1.2

--- js/drum-sampler/drum-machine.js
+++ js/drum-sampler/drum-machine.js
@@ -3,11 +3,24 @@ let mySound = new Audio();
 const keyDown = event => {
   const keyCode = event.keyCode;
 
-  // mySound.pause();
-  addClass(keyCode);
   mySound = document.querySelector(`audio[data-key='${keyCode}']`);
+  if(!mySound.paused){
+    doublePlay(mySound)
+  }
+  mySound.pause();
+  addClass(keyCode);  
   mySound.play();
-};
+}
+
+const doublePlay = Node => {
+  if(!document.querySelector(`audio[data-key='${Node.dataset.key}d']`)) {    
+    Node.insertAdjacentHTML('afterend', `<audio data-key="${Node.dataset.key}d" src="${Node.src}"></audio>`);
+  }
+  let mySound1 = new Audio();
+  mySound1 = document.querySelector(`audio[data-key='${Node.dataset.key}d']`);
+  mySound1.play();
+  mySound1.addEventListener('ended', () => mySound1.remove());
+}
 
 const addClass = keyCode => {
   const elem = document.querySelector(`div[data-key='${keyCode}']`);

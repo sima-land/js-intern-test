0a212196bc24a16421ebebdffbaeb9d9e834178e
Alex Golubev <alex@a-web.su>
2018-10-19 10:24:25 +0500

    add drum-machine.js file

---
+++ js/drum-sampler/drum-machine.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('keydown', event => {
+  const numberKey = event.keyCode;
+  const elem = [...document.querySelectorAll(`[data-key="${numberKey}"]`)];
+  elem[0].style.cssText='padding: 1.3rem 0.78rem; margin: 0.7rem';
+  HTMLAudioElement.prototype.stop = function() {
+      this.pause();
+      this.currentTime = 0.0;
+  };
+  const arrAudio = document.getElementsByTagName('audio');
+  for (let i = 0; i < arrAudio.length; i++) {
+      arrAudio[i].stop();
+  }
+  elem[1].play();
+});
+
+document.addEventListener('keyup', event => {
+  const elem = [...document.querySelectorAll(`[data-key="${event.keyCode}"]`)];
+  elem[0].style.cssText='';
+});

b278b39a3e2b6c399c9c157dd1f91e43a6bc8455
Alex Golubev <alex@a-web.su>
2018-10-19 12:42:53 +0500

    the sound is not interrupted

--- js/drum-sampler/drum-machine.js
+++ js/drum-sampler/drum-machine.js
@@ -4,13 +4,14 @@ document.addEventListener('keydown', event => {
   const elem = [...document.querySelectorAll(`[data-key="${numberKey}"]`)];
   elem[0].style.cssText='padding: 1.3rem 0.78rem; margin: 0.7rem';
   HTMLAudioElement.prototype.stop = function() {
-      this.pause();
-      this.currentTime = 0.0;
+    this.pause();
+    this.currentTime = 0.0;
   };
-  const arrAudio = document.getElementsByTagName('audio');
-  for (let i = 0; i < arrAudio.length; i++) {
-      arrAudio[i].stop();
-  }
+  // const arrAudio = document.getElementsByTagName('audio');
+  // for (let i = 0; i < arrAudio.length; i++) {
+  //     arrAudio[i].stop();
+  // }
+  elem[1].stop();
   elem[1].play();
 });
 

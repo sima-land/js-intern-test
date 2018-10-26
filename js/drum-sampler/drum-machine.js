62e15c2b7ac63292869e68434d95e0b911ec29f6
artem <artmnglv@gmail.com>
2018-10-26 18:52:35 +0500

    add drum-machine solution

---
+++ js/drum-sampler/drum-machine.js
@@ -0,0 +1,33 @@
+/**
+ * UTILS
+ */
+
+const forEachEl = (els, cb) => {
+  for (let i = 0; i < els.length; i++) {
+    cb(els[i], i);
+  }
+};
+
+/**
+ * APP
+ */
+
+const sounds = document.getElementsByTagName("audio");
+const keys = document.getElementsByClassName("key");
+
+let prevSoundIndex;
+document.onkeydown = e => {
+  forEachEl(sounds, (sound, i) => {
+    if (e.which == sound.dataset.key) {
+      keys[i].className = "key playing";
+
+      if (prevSoundIndex !== undefined && prevSoundIndex !== i) {
+        keys[prevSoundIndex].className = "key";
+        sounds[prevSoundIndex].pause();
+      }
+
+      prevSoundIndex = i;
+      sound.play();
+    }
+  });
+};

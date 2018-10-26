5a74717d4f3f3a5ef2267a58012665bcfdf4f31b
denisshytskiy <denisshytskiy@gmail.com>
2018-10-26 21:24:33 +0500

    Щуцкий Денис - решение тестового задания

--- js/search-tips/components/search-form.js
+++ js/search-tips/components/search-form.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import SearchTips from '../components/search-tips';
 
-const SearchForm =
-  () =>
-    (
-      <section>
-        <h1>Поисковые подсказки</h1>
-        <input className="search-input" type="search"/>
-        <SearchTips />
-      </section>
-    );
+const SearchForm = ({search, filter}) => (
+  <section>
+    <h1>Поисковые подсказки</h1>
+    <input className="search-input" onChange={evt => search(evt)} type="search"/>
+    {(filter.length !== 0) ? <SearchTips filter={filter}/> : null}
+  </section>
+);
 
-export default SearchForm;
+export default connect(
+  state => ({
+    filter:state.search.filter
+  }),
+  dispatch => ({
+    search: (payload) => {
+      dispatch({ type: 'SEARCH', payload:payload });
+    },
+  })
+)(SearchForm);

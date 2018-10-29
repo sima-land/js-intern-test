c74302271ef26645d3c450cffb5f47c6eee26c48
Переладов Александр <33309952+Pereladov@users.noreply.github.com>
2018-10-29 12:31:58 +0500

    update

--- js/drum-sampler/drum-machine.js
+++ js/drum-sampler/drum-machine.js
@@ -1,36 +1,35 @@
 'use strict';
 
+
+
 const KEY_CODE_ARRAY = [...document.getElementsByClassName('key')];
-const AUDIO_ARRAY = [...document.getElementsByTagName('audio')];
 
 function playSound(btnCode) {
-    let isExist = checkExistenceKey(btnCode);
-    let audio = document.querySelector(`audio[data-key="${isExist}"]`);
-    if (audio) {
-        for (let key in AUDIO_ARRAY) {
-            AUDIO_ARRAY[key].pause();
-            AUDIO_ARRAY[key].currentTime = 0;
-        }
-        audio.play()
-    }
+  let isExist = checkExistenceKey(btnCode);
+  let audio = document.querySelector(`audio[data-key="${isExist}"]`);
+  //TODO кнопка должна прерывать себя, но не влиять на звучание других.
+  if (audio.currentTime > 0) {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+  audio.play()
 }
 
 function fillBtn(btnCode) {
-    let isPushed = document.querySelector(`[data-key="${btnCode}"]`);
-    isPushed.style.cssText = 'background:red;';
-    document.addEventListener('keyup', () => isPushed.style.cssText = '')
+  let isPushed = document.querySelector(`[data-key="${btnCode}"]`);
+  isPushed.style.cssText = 'background:red;';
+  document.addEventListener('keyup', () => isPushed.style.cssText = '')
 }
 
 function checkExistenceKey(btnCode) {
-    for (let key in KEY_CODE_ARRAY) {
-        if (btnCode === parseInt(KEY_CODE_ARRAY[key].getAttribute('data-key'))) {
-            fillBtn(btnCode);
-            return btnCode
-        }
+  for (let key in KEY_CODE_ARRAY) {
+    if (btnCode === parseInt(KEY_CODE_ARRAY[key].getAttribute('data-key'))) {
+      fillBtn(btnCode);
+      return btnCode
     }
+  }
 }
 
 document.addEventListener('keydown', e => {
-    playSound(e.keyCode);
+  playSound(e.keyCode);
 });
-

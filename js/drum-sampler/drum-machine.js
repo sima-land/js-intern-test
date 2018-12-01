f3a1574470e4204133f4278598f5c18a071a7081
LazzyMind <hazipov.vlad@yandex.ru>
2018-12-01 23:17:19 +0500

    fix drum machine issues

--- js/drum-sampler/drum-machine.js
+++ js/drum-sampler/drum-machine.js
@@ -1,18 +1,20 @@
-const audios = [...document.getElementsByTagName("audio")]
-const plates = [...document.querySelectorAll(".key")]
-const keys = plates.map(plate => plate.dataset.key)
-
-window.addEventListener("keydown", playMisic)
+const audios = [...document.getElementsByTagName("audio")];
+const plates = [...document.querySelectorAll(".key")];
+const keys = plates.map((plate) => plate.dataset.key);
 
 function playMisic(event) {
-  const keyCode = event.keyCode.toString()
-  if(keys.indexOf(keyCode) === -1) return
-
-  audios.forEach(audio => {audio.load()})
-  const audioToPlay = audios.find(audio => audio.dataset.key === keyCode)
-  audioToPlay.play()
+  const keyCode = event.keyCode.toString();
+  if (keys.indexOf(keyCode) === -1) {
+    return;
+  }
   
-  plates.forEach(plate => {plate.classList.remove("playing")})
-  const plateToShow = plates.find(plate => plate.dataset.key === keyCode)
+  audios.forEach(audio => {audio.load()});
+  const audioToPlay = audios.find((audio) => audio.dataset.key === keyCode);
+  audioToPlay.play();
+  
+  plates.forEach(plate => {plate.classList.remove("playing")});
+  const plateToShow = plates.find((plate) => plate.dataset.key === keyCode);
   plateToShow.classList.add("playing")
 }
+
+window.addEventListener("keydown", playMisic);

ca0c0e98308ad6f7956960f42a7862eb9583c4eb
Vasily <vlobarev@gmail.com>
2020-06-04 23:25:17 +1000

    standarts fixes

--- js/highlight/highlight.js
+++ js/highlight/highlight.js
@@ -7,7 +7,7 @@
 function highlight(
     sourceHtml,
     plainTextSubstring,
-    highlighter = entry => `<b>${entry}</b>`
+    highlighter = (entry) => `<b>${entry}</b>`
 ) {
     let count = 0;
     let indexes = [];
@@ -22,7 +22,7 @@ function highlight(
             let request = plainTextSubstring;
             if (nextSearch) {
                 request = nextSearch;
-                nextSearch = undefined
+                nextSearch = undefined;
             }
 
             let foundIndex = nodeVal.toLowerCase().indexOf(request.toLowerCase());
@@ -31,26 +31,26 @@ function highlight(
                 if (
                     !indexes.length ||
                     indexes[0] - count >= nodeVal.length ||
-                    (nextNode != null &&
-                        (nextNode.nodeName === 'SCRIPT' || nextNode.nodeName === 'STYLE'))
+                    (nextNode !== null &&
+                        (nextNode.nodeName === "SCRIPT" || nextNode.nodeName === "STYLE"))
                 ) {
                     if (
-                        nextNode != null &&
-                        (nextNode.nodeName === 'SCRIPT' || nextNode.nodeName === 'STYLE')
+                        nextNode !== null &&
+                        (nextNode.nodeName === "SCRIPT" || nextNode.nodeName === "STYLE")
                     ) {
-                        indexes.shift()
+                        indexes.shift();
                     }
 
                     if (isFirst) {
                         count += nodeVal.length;
-                        break
+                        break;
                     }
                     if (nodeVal) {
                         let textNode = document.createTextNode(nodeVal);
                         count += nodeVal.length;
-                        parentNode.insertBefore(textNode, node)
+                        parentNode.insertBefore(textNode, node);
                     }
-                    parentNode.removeChild(node)
+                    parentNode.removeChild(node);
                 } else {
                     if (indexes[0] - count > 0) {
                         let textNode = document.createTextNode(
@@ -65,12 +65,12 @@ function highlight(
                     );
                     newNode.innerHTML = highlighter(nodeVal.substring(indexes[0] - count));
                     for (let child of Array.prototype.slice.call(newNode.childNodes)) {
-                        parentNode.insertBefore(child, node)
+                        parentNode.insertBefore(child, node);
                     }
                     count += indexes[0] - count + request.length - addLength;
                     indexes.shift();
                     indexes.unshift(count);
-                    parentNode.removeChild(node)
+                    parentNode.removeChild(node);
                 }
                 break
             }
@@ -81,39 +81,40 @@ function highlight(
                 parentNode.insertBefore(textNode, node)
             }
 
-            let newNode = document.createElement('div');
+            let newNode = document.createElement("div");
             newNode.innerHTML = highlighter(
                 nodeVal.substring(foundIndex, foundIndex + request.length)
             );
             for (let child of Array.prototype.slice.call(newNode.childNodes)) {
-                parentNode.insertBefore(child, node)
+                parentNode.insertBefore(child, node);
             }
             count += foundIndex + request.length;
             indexes.shift();
-            nodeVal = nodeVal.substring(foundIndex + request.length)
+            nodeVal = nodeVal.substring(foundIndex + request.length);
         }
     }
 
     function loopDOMElementsC(elementDOM) {
-        if (elementDOM === null) return;
+        if (elementDOM === null) {
+            return;
+        }
         let children = Array.prototype.slice.call(elementDOM.childNodes);
         if (children.length) {
-            for (let i = 0; i < children.length; i++) {
-                let currentChild = children[i];
+            for (let currentChild of children) {
                 if (currentChild.nodeType === Node.TEXT_NODE) {
                     if (indexes[0] - count < currentChild.nodeValue.length) {
-                        checkAndReplaceC(currentChild)
+                        checkAndReplaceC(currentChild);
                     } else {
-                        count += currentChild.nodeValue.length
+                        count += currentChild.nodeValue.length;
                     }
                 } else if (currentChild.nodeType === Node.ELEMENT_NODE) {
                     if (
-                        currentChild.nodeName === 'SCRIPT' ||
-                        currentChild.nodeName === 'STYLE'
+                        currentChild.nodeName === "SCRIPT" ||
+                        currentChild.nodeName === "STYLE"
                     ) {
-                        count += currentChild.textContent.length
+                        count += currentChild.textContent.length;
                     } else {
-                        loopDOMElementsC(currentChild)
+                        loopDOMElementsC(currentChild);
                     }
                 }
             }
@@ -129,21 +130,21 @@ function highlight(
                 .indexOf(plainTextSubstring.toLowerCase(), startIndex);
             if (foundIndex !== -1) {
                 indexes.push(foundIndex);
-                startIndex = foundIndex + plainTextSubstring.length
+                startIndex = foundIndex + plainTextSubstring.length;
             } else {
                 break
             }
         }
         if (indexes.length) {
-            loopDOMElementsC(elementDOM, indexes)
+            loopDOMElementsC(elementDOM, indexes);
         }
     }
 
-    let tempDOMElement = document.createElement('div');
+    let tempDOMElement = document.createElement("div");
     tempDOMElement.innerHTML = sourceHtml;
 
     tryToFind(tempDOMElement);
-    return tempDOMElement.innerHTML
+    return tempDOMElement.innerHTML;
 }
 
 module.exports = highlight;
